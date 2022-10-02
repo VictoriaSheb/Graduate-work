5d24d2253b1a5d347061becdca3978ed48d5c6d8
VictoriaSheb <71636310+VictoriaSheb@users.noreply.github.com>
2022-10-02 23:49:45 +0300

    Minesweeper Game

---
+++ GameSaper/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace GameSaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mINewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mIClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ml = new System.Windows.Forms.ToolStripMenuItem();
+            this.mIAboutProgram = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lbWin = new System.Windows.Forms.Label();
+            this.lbLose = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(66, 69);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.56522F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(455, 449);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.ml});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(574, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mINewGame,
+            this.toolStripSeparator1,
+            this.mIClose});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(65, 24);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // mINewGame
+            // 
+            this.mINewGame.Name = "mINewGame";
+            this.mINewGame.Size = new System.Drawing.Size(224, 26);
+            this.mINewGame.Text = "Новая игра";
+            this.mINewGame.Click += new System.EventHandler(this.mINewGame_Click);
+            // 
+            // mIClose
+            // 
+            this.mIClose.Name = "mIClose";
+            this.mIClose.Size = new System.Drawing.Size(224, 26);
+            this.mIClose.Text = "Выход";
+            this.mIClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // ml
+            // 
+            this.ml.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mIAboutProgram});
+            this.ml.Name = "ml";
+            this.ml.Size = new System.Drawing.Size(81, 24);
+            this.ml.Text = "Справка";
+            // 
+            // mIAboutProgram
+            // 
+            this.mIAboutProgram.Name = "mIAboutProgram";
+            this.mIAboutProgram.Size = new System.Drawing.Size(224, 26);
+            this.mIAboutProgram.Text = "О программе";
+            this.mIAboutProgram.Click += new System.EventHandler(this.mIAboutProgram_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(401, 28);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(46, 17);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.Text = "label1";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(443, 553);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(87, 33);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Выход";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lbWin
+            // 
+            this.lbWin.AutoSize = true;
+            this.lbWin.Location = new System.Drawing.Point(12, 573);
+            this.lbWin.Name = "lbWin";
+            this.lbWin.Size = new System.Drawing.Size(54, 17);
+            this.lbWin.TabIndex = 4;
+            this.lbWin.Text = "Побед:";
+            // 
+            // lbLose
+            // 
+            this.lbLose.AutoSize = true;
+            this.lbLose.Location = new System.Drawing.Point(178, 573);
+            this.lbLose.Name = "lbLose";
+            this.lbLose.Size = new System.Drawing.Size(46, 17);
+            this.lbLose.TabIndex = 5;
+            this.lbLose.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 573);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 599);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbLose);
+            this.Controls.Add(this.lbWin);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Сапер";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mINewGame;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mIClose;
+        private System.Windows.Forms.ToolStripMenuItem ml;
+        private System.Windows.Forms.ToolStripMenuItem mIAboutProgram;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lbWin;
+        private System.Windows.Forms.Label lbLose;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GameSaper/Form1.cs
@@ -0,0 +1,82 @@
+﻿using LibSaper;
+using System;
+using System.Windows.Forms;
+
+namespace GameSaper
+{
+    public partial class Form1 : Form
+    {
+        int countLose;
+        int countWin;
+
+        ClassAllCells field;
+        public Form1()
+        {
+            InitializeComponent();
+            countLose = countWin = 0;
+            field = new ClassAllCells();
+            FillInTheTableCells(9);
+            field.LoseGameEvent += LoseGame;
+            field.WinGameEvent += WinGame;
+            lbLose.Text = "Поражений: " + countLose.ToString();
+            lbWin.Text = "Побед: " + countWin.ToString();
+        }
+
+        private void LoseGame() 
+        {
+            MessageBox.Show("Игра окончена(");
+            lbResult.Text = "Результат: проигрыш";
+            countLose++;
+            lbLose.Text = "Поражений: " + countLose.ToString();
+        }
+
+        private void WinGame()
+        {
+            MessageBox.Show("Вы победили!!!!");
+            lbResult.Text = "Результат: победа";
+            countWin++;
+            lbWin.Text = "Побед: " + countWin.ToString();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mINewGame_Click(object sender, EventArgs e)
+        {
+            field = new ClassAllCells();
+            tableLayoutPanel1.Controls.Clear();
+            FillInTheTableCells(9); 
+        }
+
+
+        private void FillInTheTableCells(int x)
+        {
+            lbResult.Text = "";
+            tableLayoutPanel1.ColumnCount = x;
+            tableLayoutPanel1.RowCount = x;
+            tableLayoutPanel1.Height = 48 * x;
+            tableLayoutPanel1.Width = 48 * x;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    tableLayoutPanel1.Controls.Add(field.cells[i, j], i, j);
+                }
+                tableLayoutPanel1.RowStyles[i].Height = 48;
+                tableLayoutPanel1.ColumnStyles[i].Width = 48;
+            }
+        }
+
+        private void mIAboutProgram_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Правила игры следующие:                                                                                   " +
+              "Игровое поле состоит из клеток, в каждой из которых может быть мина.Мины размещаются в поле случайным образом.Задача играющего – найти все мины и пометить их флажками.Используя кнопки мыши, игрок может открыть клетку или поставить в нее флажок, указав тем самым, что в клетке обнаружена мина.Клетка открывается щелчком левой кнопки мыши, а флажок ставится щелчком правой." +
+              "Если в клетке, которую открыл игрок, есть мина, то происходит взрыв и игра заканчивается поражением игрока.Если в клетке мины нет, то в этой клетке появляется число, соответствующее количеству мин, находящихся в соседних клетках." +
+              "Анализируя информацию о количестве мин в клетках, соседних с уже открытыми, игрок может обнаружить и пометить флажками все мины.Ограничений на количество клеток, помеченных флажками, нет.Ошибочно установленный флажок можно убрать, щелкнув правой кнопкой мыши в клетке, в которой он находится." +
+              "Для удачного завершения игры(выигрыша) флажки должны быть установлены только в тех клетках, в которых есть мины." +
+              "");
+        }
+    }
+}
